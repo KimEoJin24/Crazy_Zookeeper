a20ee71e02ba6a8bbff03f58ad3ec8d2a4f9c3af
tlqdbsrud <141490250+tlqdbsrud@users.noreply.github.com>
2023-10-16 19:27:42 +0900

    [Update]Player Move

--- Crazy_Zookeeper/Assets/Scripts/Stage3/Player/PlayerController.cs
+++ Crazy_Zookeeper/Assets/Scripts/Stage3/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,18 +13,82 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayerMask;
 
     [Header("Look")]
-    public Transform cameraContaniner;
-    public float minxLook;
+    public Transform cameraContainer;
+    public float minXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensitivity;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 mouseDelta;
+
+    [HideInInspector]
+    public bool canLook = true;
+
+    private Rigidbody _rigidbody;
+
+    // 싱글톤
+    public static PlayerController instance;
+
+    private void Awake()
     {
-        
+        instance = this;
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // 물리적 업데이트
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    // 카메라
+    private void LateUpdate()
+    {
+        if (canLook)
+        {
+            CameraLook();
+        }
+    }
+
+    // 이동
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = _rigidbody.velocity.y;
+        _rigidbody.velocity = dir;
+    }
+
+    // 마우스 카메라 
+    void CameraLook()
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+
+    // 마우스 이벤트
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed) // 실행
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if(context.phase == InputActionPhase.Canceled) // 취소
+        {
+            curMovementInput = Vector2.zero;
+        }
     }
 }
