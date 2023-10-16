f79abfb0fda75a4ce89010053b40b0d34bf5501f
tlqdbsrud <141490250+tlqdbsrud@users.noreply.github.com>
2023-10-16 17:50:36 +0900

    [Update]Player
    InputAction

--- Crazy_Zookeeper/Assets/Scripts/Stage3/Player/PlayerController.cs
+++ Crazy_Zookeeper/Assets/Scripts/Stage3/Player/PlayerController.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+    public float jumpForce;
+    public LayerMask groundLayerMask;
+
+    [Header("Look")]
+    public Transform cameraContaniner;
+    public float minxLook;
+
     // Start is called before the first frame update
     void Start()
     {
