797f22ce4eec8345518564a54ec8cbcf699abf48
tlqdbsrud <141490250+tlqdbsrud@users.noreply.github.com>
2023-10-18 15:44:46 +0900

    [ADD] NavaMesh

--- Crazy_Zookeeper/Assets/Scripts/Stage3/Zookeeper(Enemy)/PatrolState.cs
+++ Crazy_Zookeeper/Assets/Scripts/Stage3/Zookeeper(Enemy)/PatrolState.cs
@@ -1,3 +1,4 @@
+using UnityEngine.AI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,24 @@ using UnityEngine;
 public class PatrolState : StateMachineBehaviour
 {
     float timer;
+    List<Transform> wayPoints = new List<Transform>();
+    NavMeshAgent agent;
+
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        agent = animator.GetComponent<NavMeshAgent>();
         timer = 0;
+        GameObject go = GameObject.FindGameObjectWithTag("WayPoints");
+        foreach(Transform t in go.transform)
+            wayPoints.Add(t);
+        agent.SetDestination(wayPoints[Random.Range(0, wayPoints.Count)].position);
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if(agent.remainingDistance <= agent.stoppingDistance)
+            agent.SetDestination(wayPoints[Random.Range(0, wayPoints.Count)].position);
+        
         timer += Time.deltaTime;
         if (timer > 5)
             animator.SetBool("isPatrolling", false);
@@ -20,7 +32,7 @@ public class PatrolState : StateMachineBehaviour
     
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        agent.SetDestination(agent.transform.position);
     }
 
     override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
