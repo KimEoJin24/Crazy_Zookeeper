32f8dc6a7f75199a58845e0f4423e3f501fa3252
M1NO001 <dntjd1306@naver.com>
2023-10-18 22:48:46 +0900

    [FIX]Conflict

--- Crazy_Zookeeper/Assets/PanelHandler.cs
+++ Crazy_Zookeeper/Assets/PanelHandler.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PanelHandler : MonoBehaviour
+
 {
     // Start is called before the first frame update
     void Start()
