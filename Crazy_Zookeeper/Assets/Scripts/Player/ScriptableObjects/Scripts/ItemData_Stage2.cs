b6c828939e07597c8633c22e21c7089b4f113f4d
M1NO001 <dntjd1306@naver.com>
2023-10-18 20:26:25 +0900

    [Chore]Asset

--- Crazy_Zookeeper/Assets/Scripts/Player/ScriptableObjects/Scripts/ItemData_Stage2.cs
+++ Crazy_Zookeeper/Assets/Scripts/Player/ScriptableObjects/Scripts/ItemData_Stage2.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class ItemData_Stage2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
