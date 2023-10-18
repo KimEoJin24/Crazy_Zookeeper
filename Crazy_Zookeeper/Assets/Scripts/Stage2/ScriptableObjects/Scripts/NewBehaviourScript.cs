98f07ce398c37c524edaa4d87773693bed3b6d6e
M1NO001 <dntjd1306@naver.com>
2023-10-18 22:59:40 +0900

    [FIX]Conflict

--- Crazy_Zookeeper/Assets/Scripts/Stage2/ScriptableObjects/Scripts/NewBehaviourScript.cs
+++ Crazy_Zookeeper/Assets/Scripts/Stage2/ScriptableObjects/Scripts/NewBehaviourScript.cs
@@ -1,11 +1,9 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class PanelHandler : MonoBehaviour
-
-
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -19,3 +17,4 @@ public class PanelHandler : MonoBehaviour
         
     }
 }
+
