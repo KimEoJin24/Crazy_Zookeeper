c1b3fb60cb50d16eb66e44c87ea3c91d4a960273
KimEoJin24 <kamejk24@gmail.com>
2023-10-18 18:14:39 +0900

    [ADD] MenuManager

---
+++ Crazy_Zookeeper/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menuUI;
+    public static bool menuUITF = false;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0.0f;
+            menuUI.SetActive(true);
+            menuUITF = true;
+        }
+    }
+    
+    public void PlayButton()
+    {
+        Time.timeScale = 1.0f;
+        menuUI.SetActive(false);
+        menuUITF = false;   
+    }
+
+    public void ExitButton()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("StartScene");
+        menuUI.SetActive(false);   
+        menuUITF = false;   
+    }
+
+}
