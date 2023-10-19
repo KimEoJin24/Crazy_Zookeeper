9ded9761f8c13ca73050cd11ce181f39a2ce3d4e
MAXracoon <143587600+MAXracoon@users.noreply.github.com>
2023-10-19 14:48:21 +0900

    stage1 fail

---
+++ Crazy_Zookeeper/Assets/Scripts/Stage1/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private string nextSceneName;
+    [SerializeField]
+    private GameObject panelStageClear;
+    private float GameTime = 7;
+    public Text GameTimeText;
+
+    private void Awake()
+    {
+        Time.timeScale = 1.0f;
+        panelStageClear.SetActive(false);
+    }
+        private void Update()
+    {
+      
+        if((int)GameTime ==0)
+        {
+            Debug.Log("게임종료");
+            Time.timeScale = 0.0f;
+            panelStageClear.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+
+        else
+        {
+            GameTime -= Time.deltaTime;
+            Debug.Log((int)GameTime);
+            GameTimeText.text = "Time: " + (int)GameTime;
+        }
+    }
+}
