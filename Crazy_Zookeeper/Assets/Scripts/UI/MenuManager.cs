c59ab3f47577f4d19b2558740de0cc636c3f8b88
KimEoJin24 <kamejk24@gmail.com>
2023-10-19 22:20:45 +0900

    [Update] MenuManager

--- Crazy_Zookeeper/Assets/Scripts/UI/MenuManager.cs
+++ Crazy_Zookeeper/Assets/Scripts/UI/MenuManager.cs
@@ -14,17 +14,22 @@ public class MenuManager : MonoBehaviour
             if(menuUI.activeSelf)
             {
                 menuUI.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
             }
             else
             {
                 menuUI.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
             }
         }
     }
     public void ExitButton()
     {
         SceneManager.LoadScene("StartScene");
-
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
 }
