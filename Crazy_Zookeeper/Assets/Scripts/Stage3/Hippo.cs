ac5122661316eae84155778dc3921c7abfd425bf
tlqdbsrud <141490250+tlqdbsrud@users.noreply.github.com>
2023-10-19 21:50:19 +0900

    [ADD] HP UI

--- Crazy_Zookeeper/Assets/Scripts/Stage3/Hippo.cs
+++ Crazy_Zookeeper/Assets/Scripts/Stage3/Hippo.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hippo : MonoBehaviour
 {
     public int HP = 100;
+    private int maxHP = 100; // 최대값(100으로 설정)
+
     public Animator animator;
+    public Image healthBar;
+
+    private void Start()
+    {
+        UpdateHealthBar(); // 게임 시작 시 HP바 초기화
+    }
+
+    private void Update()
+    {
+        UpdateHealthBar(); // HP를 0에서 1로 정규화하여 HP바 업데이트
+    }
+    private void UpdateHealthBar()
+    {
+        healthBar.fillAmount = (float)HP / maxHP; // 정규화한 값으로 HP바 업데이트
+    }
 
     public void TakeDamage(int damageAmount)
     {
@@ -14,6 +32,7 @@ public class Hippo : MonoBehaviour
         if (HP <= 0)
         {
             animator.SetTrigger("Die");
+            GetComponent<Collider>().enabled = false;
             Debug.Log(HP + "Hippo Die");
         }
         else
@@ -21,6 +40,8 @@ public class Hippo : MonoBehaviour
             animator.SetTrigger("Damage");
             Debug.Log(HP + "Hippo Damage");
         }
+
+        UpdateHealthBar(); // 데미지 입었을 때 HP바 업데이트
     }
 
     private void OnTriggerEnter(Collider other)
--- Crazy_Zookeeper/Assets/Scripts/Stage3/Zookeeper(Enemy)/Zookeeper.cs
+++ Crazy_Zookeeper/Assets/Scripts/Stage3/Zookeeper(Enemy)/Zookeeper.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Zookeeper : MonoBehaviour
 {
-    public int HP = 100;
+    private int HP = 100;
+    private int maxHP = 100; // 최대값(100으로 설정)
+
     public Animator animator;
+    public Image healthBar;
+
+    private void Start()
+    {
+        UpdateHealthBar(); // 게임 시작 시 HP바 초기화
+    }
+
+    private void Update()
+    {
+        UpdateHealthBar(); // HP를 0에서 1로 정규화하여 HP바 업데이트
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBar.fillAmount = (float)HP / maxHP; // 정규화한 값으로 HP바 업데이트
+    }
 
     public void TakeDamage(int damageAmount)
     {
         HP -= damageAmount;
-        ZookeeperConditions.Instance.TakePhysicalDamage(damageAmount);
+        //ZookeeperConditions.Instance.TakePhysicalDamage(damageAmount);
         if (HP <= 0)
         {
             animator.SetTrigger("Die");
+            GetComponent<Collider>().enabled = false;
             Debug.Log(HP + "Die");
         }
         else
@@ -21,5 +41,7 @@ public class Zookeeper : MonoBehaviour
             animator.SetTrigger("Damage");
             Debug.Log(HP + "Damage");
         }
+
+        UpdateHealthBar(); // 데미지 입었을 때 HP바 업데이트
     }
 }
