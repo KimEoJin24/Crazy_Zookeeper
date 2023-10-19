443b2a4ebffc0a2b36de0f9da3fdb00326b0b0e5
KimEoJin24 <kamejk24@gmail.com>
2023-10-19 17:01:56 +0900

    [Fix] Menu

--- Crazy_Zookeeper/Assets/Scripts/UI/MenuManager.cs
+++ Crazy_Zookeeper/Assets/Scripts/UI/MenuManager.cs
@@ -10,24 +10,20 @@ public class MenuManager : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && menuUITF == false)
+        if (Input.GetKeyDown(KeyCode.Tab) && menuUITF == false)
         {
-            Time.timeScale = 0.0f;
             menuUI.SetActive(true);
-            menuUITF = true;
         }
     }
     
     public void PlayButton()
     {
-        Time.timeScale = 1.0f;
         menuUI.SetActive(false);
         menuUITF = false;   
     }
 
     public void ExitButton()
     {
-        Time.timeScale = 1.0f;
         SceneManager.LoadScene("StartScene");
         menuUI.SetActive(false);   
         menuUITF = false;   
