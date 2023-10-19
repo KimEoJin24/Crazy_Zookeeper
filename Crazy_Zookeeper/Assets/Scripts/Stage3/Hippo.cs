8deda4c5473361a418fe23c2dd77c8e56c9c896a
tlqdbsrud <141490250+tlqdbsrud@users.noreply.github.com>
2023-10-19 20:52:47 +0900

    [Update]Potion 수정

--- Crazy_Zookeeper/Assets/Scripts/Stage3/Hippo.cs
+++ Crazy_Zookeeper/Assets/Scripts/Stage3/Hippo.cs
@@ -23,8 +23,27 @@ public class Hippo : MonoBehaviour
         }
     }
 
-    public void PickItem(int hp)
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Green"))
+        {
+            IncreaseHealth(20);
+            Destroy(other.gameObject); // "Green" 아이템을 먹었으므로 해당 오브젝트를 제거
+        }
+        else if (other.CompareTag("Blue"))
+        {
+            IncreaseHealth(30);
+            Destroy(other.gameObject); // "Blue" 아이템을 먹었으므로 해당 오브젝트를 제거
+        }
+        else if (other.CompareTag("Red"))
+        {
+            IncreaseHealth(40);
+            Destroy(other.gameObject); // "Red" 아이템을 먹었으므로 해당 오브젝트를 제거
+        }
+    }
+    private void IncreaseHealth(int amount)
+    {
+        HP += amount;
+        Debug.Log("Hippo's HP increased by " + amount + ". Current HP: " + HP);
     }
 }
